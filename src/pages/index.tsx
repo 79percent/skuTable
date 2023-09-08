2bcc911ad635a02acfac1369fe1a9f68efeb9fa3
gechaofeng <gechaofeng@jiuxiniot.com>
2023-09-08 15:45:20 +0800

    feat: 0908

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
 import {
 	ProForm,
-	ProFormDependency,
-	ProFormList,
-	ProFormText,
 	ProCard,
 	ProFormSelect,
 	ProTable,
+	ProFormList,
+	ProFormGroup,
+	ProFormText,
 } from "@ant-design/pro-components";
-import { Alert, Button, Col, Form, Row, Space } from "antd";
-import type { ProFormInstance } from "@ant-design/pro-components";
-import { useRef, useState } from "react";
+import { Alert, Button, Col, Form, Row } from "antd";
+import { useState } from "react";
 import { randomString, attrNameList, attrValueList } from "./utils";
 import styles from "./index.less";
 import type { ProColumns } from "@ant-design/pro-components";
+import { CloseCircleOutlined, SmileOutlined } from "@ant-design/icons";
 
 const originColumns: ProColumns<any>[] = [
 	{
@@ -44,164 +44,48 @@ const originColumns: ProColumns<any>[] = [
 ];
 
 export default function HomePage() {
-	const formRef = useRef<ProFormInstance>();
 	const [form] = Form.useForm();
 	const [editableForm] = Form.useForm();
 	const [skuKeyList, setSkuKeyList] = useState<any[]>([]);
 	const [dataSource, setDataSource] = useState<any[]>([]);
 	const [activeColumns, setActiveColumns] = useState<any[]>([]);
-	const editableFormValuesRef = useRef<any>({});
 	const [alertVisile, setAlertVisile] = useState({
 		show: false,
 		msg: "",
 	});
 
-	const editableKeys = dataSource.map((item) => item.id);
+	const editableKeys = dataSource.map((item) => item?.id);
+
+	console.log(dataSource);
 
 	const columns = [...activeColumns, ...originColumns];
 
-	// console.log(columns)
-	// console.log(dataSource)
-
-	// 添加规格项
-	const handleAddSkuItem = () => {
-		const key = `new_${randomString(20)}`;
-		const currentIndex = skuKeyList.length;
-		setSkuKeyList([...skuKeyList, key]);
-		setActiveColumns([
-			...activeColumns,
-			{
-				title: "#",
-				dataIndex: `#${currentIndex}`,
-				editable: false,
-        render: (_: any, record: any) => {
-          const { skuList } = record;
-          return Array.isArray(skuList) && skuList[currentIndex]?.label || '-';
-        }
-			},
-		]);
-		if (dataSource.length === 0) {
-			const dataId = randomString(20);
-			setDataSource([
-				{
-					id: dataId,
-					oprice: undefined,
-					cprice: undefined,
-					stock: undefined,
-				},
-			]);
-		}
-	};
-
-	// 选择规格名
-	const handleSelectAttrName = (item: any, index: number) => {
-		const copyActiveColumns = [...activeColumns];
-		copyActiveColumns[index].title = item.label;
-		copyActiveColumns[index].dataIndex = item.value;
-		setActiveColumns(copyActiveColumns);
-	};
-
-	// 删除规格项
-	const handleDeleteSkuItem = (index: number) => {
-		const copyActiveColumns = [...activeColumns];
-		copyActiveColumns.splice(index, 1);
-		setActiveColumns(copyActiveColumns);
-		const copySkuKeyList = [...skuKeyList];
-		copySkuKeyList.splice(index, 1);
-		setSkuKeyList(copySkuKeyList);
-		if (copySkuKeyList.length === 0) {
-			setDataSource([]);
-		}
-	};
-
-  const createSkuData = () => {
-    const values = form?.getFieldsValue();
-    const skuList = skuKeyList.map((key, index) => {
+	// 根据所选规格生成排列组合数据
+	const createSkuData = (v?: any, inputKeyList?: any[]) => {
+		const values = v || form?.getFieldsValue();
+		const keyList = inputKeyList || skuKeyList;
+		const skuList = keyList.map((key, index) => {
 			const formAttrName = `attrName_${key}`;
 			const formAttrValue = `attrValue_${key}`;
-			return {
-				attrName: values[formAttrName],
-				attrValue: values[formAttrValue] || [],
-			};
-		});
-		console.log("skuList", skuList);
-    const skuMatrix = skuList.map((sku, index) => {
-			const { attrValue } = sku;
-			return attrValue;
-		});
-		console.log("skuMatrix", skuMatrix);
-    let i = skuMatrix.length - 1;
-		while (i >= 1) {
-			const itemArr = skuMatrix[i];
-			const prevItemArr = skuMatrix[i - 1];
-			prevItemArr.forEach((item: any) => {
-				item.chileren = itemArr;
+			const attrName = values[formAttrName];
+			let attrValue: any[] = values[formAttrValue] || [];
+			attrValue = attrValue.map((item) => {
+				return {
+					...item,
+					chileren: [],
+				};
 			});
-			i--;
-		}
-		const skuTree = skuMatrix[0] || [];
-		console.log("skuTree", skuTree);
-    const skuData: any[] = [];
-		function recursion(node: any, path: any[]) {
-			const { chileren, ...restNode } = node;
-			path.push(restNode);
-			if (Array.isArray(chileren) && chileren.length > 0) {
-				chileren.forEach((item: any) => {
-					recursion(item, [...path]);
-				});
-			} else {
-				skuData.push(path);
-			}
-		}
-		skuTree.forEach((node: any) => {
-			recursion(node, []);
-		});
-		console.log("skuData", skuData);
-    return skuData;
-  }
-
-	// 添加规格值
-	const handleAddAttrValue = (item: any, index: number) => {
-    console.log('handleAddAttrValue', item, index)
-    const skuData = createSkuData();
-    setDataSource(skuData.map((skuList, index) => {
-      return {
-        ...dataSource[index],
-        skuList,
-      }
-    }));
-	};
-
-	// 删除规格值
-	const handleDeleteAttrValue = (item: any, index: number) => {};
-
-	const createSkuList = (values: any, skuKeyList: any[]) => {
-		const skuList = skuKeyList.map((key, index) => {
-			const formAttrName = `attrName_${key}`;
-			const formAttrValue = `attrValue_${key}`;
 			return {
-				attrName: values[formAttrName],
-				attrValue: values[formAttrValue] || [],
+				attrName,
+				attrValue,
 			};
 		});
-		console.log("skuList", skuList);
-		const newActiveColumns: ProColumns<any>[] = [];
-		skuList.forEach((sku) => {
-			if (sku?.attrName) {
-				newActiveColumns.push({
-					title: sku.attrName.label,
-					dataIndex: `attrName_${sku.attrName.value}`,
-					editable: false,
-				});
-			}
-		});
-		console.log("newActiveColumns", newActiveColumns);
-		setActiveColumns(newActiveColumns);
+		// console.log("skuList", skuList);
 		const skuMatrix = skuList.map((sku, index) => {
 			const { attrValue } = sku;
 			return attrValue;
 		});
-		console.log("skuMatrix", skuMatrix);
+		// console.log("skuMatrix", skuMatrix);
 		let i = skuMatrix.length - 1;
 		while (i >= 1) {
 			const itemArr = skuMatrix[i];
@@ -212,7 +96,7 @@ export default function HomePage() {
 			i--;
 		}
 		const skuTree = skuMatrix[0] || [];
-		console.log("skuTree", skuTree);
+		// console.log("skuTree", skuTree);
 		const skuData: any[] = [];
 		function recursion(node: any, path: any[]) {
 			const { chileren, ...restNode } = node;
@@ -229,70 +113,215 @@ export default function HomePage() {
 			recursion(node, []);
 		});
 		console.log("skuData", skuData);
-		const editableValues = editableFormValuesRef.current;
-		console.log("editableValues", editableValues);
-		const newDataSource = skuData.map((item: any[], i) => {
-			let obj: any = {
-				oprice: undefined,
-				cprice: undefined,
-				stock: undefined,
+		return skuData;
+	};
+
+	// 添加规格项
+	const handleAddSkuItem = async () => {
+		await form.validateFields();
+		const key = `new_${randomString(20)}`;
+		const currentIndex = skuKeyList.length;
+		setSkuKeyList([...skuKeyList, key]);
+		setActiveColumns([
+			...activeColumns,
+			{
+				title: "#",
+				dataIndex: `#${currentIndex}`,
+				editable: false,
+				render: (_: any, record: any) => {
+					const { skuList } = record;
+					return (
+						(Array.isArray(skuList) && skuList[currentIndex]?.label) || "-"
+					);
+				},
+			},
+		]);
+		if (dataSource.length === 0) {
+			const dataId = randomString(20);
+			setDataSource([
+				{
+					id: dataId,
+					oprice: undefined,
+					cprice: undefined,
+					stock: undefined,
+					skuList: [],
+				},
+			]);
+		}
+	};
+
+	// 选择规格名
+	const handleSelectAttrName = (item: any, index: number) => {
+		const copyActiveColumns = [...activeColumns];
+		copyActiveColumns[index].title = item.label;
+		copyActiveColumns[index].dataIndex = item.value;
+		setActiveColumns(copyActiveColumns);
+	};
+
+	// 删除规格项
+	const handleDeleteSkuItem = (index: number) => {
+		let copyActiveColumns = [...activeColumns];
+		copyActiveColumns.splice(index, 1);
+		copyActiveColumns = copyActiveColumns.map((item, i) => {
+			return {
+				...item,
+				dataIndex: `#${i}`,
+				render: (_: any, record: any) => {
+					const { skuList } = record;
+					return (Array.isArray(skuList) && skuList[i]?.label) || "-";
+				},
 			};
-			let keyArr: any[] = [];
-			let attrList: any[] = [];
-			item.forEach((sku: any, j) => {
-				let { dataIndex, title } = newActiveColumns[j];
-				dataIndex = String(dataIndex);
-				const [_, attrId] = dataIndex.split("_");
-				attrList.push({
-					attrName: title,
-					attrNameId: Number(attrId),
-					attrValue: sku.label,
-					attrValueId: Number(sku.value),
-				});
-				keyArr.push(skuKeyList[j]);
-				obj = {
-					...obj,
-					[dataIndex]: sku.label,
-					attrList,
+		});
+		setActiveColumns(copyActiveColumns);
+		const copySkuKeyList = [...skuKeyList];
+		copySkuKeyList.splice(index, 1);
+		setSkuKeyList(copySkuKeyList);
+		if (copySkuKeyList.length === 0) {
+			setDataSource([]);
+		} else {
+			const formAttrName = `attrName_${skuKeyList[index]}`;
+			const formAttrValue = `attrValue_${skuKeyList[index]}`;
+			const values = form?.getFieldsValue();
+			delete values[formAttrName];
+			delete values[formAttrValue];
+			const skuData = createSkuData(values, copySkuKeyList);
+			let copyDataSource = [...dataSource];
+			copyDataSource.map((item) => {
+				let itemSkuList: any[] = item?.skuList || [];
+				itemSkuList = itemSkuList.splice(index, 1);
+				return {
+					...item,
+					skuList: itemSkuList,
 				};
 			});
-			let key = `${keyArr.join("-")}_SkuRowKey_${JSON.stringify(attrList)}`;
-			obj.key = key;
-			let oldItemObj = editableValues[key];
-			if (oldItemObj) {
-				obj = {
-					...obj,
-					...oldItemObj,
+			copyDataSource = skuData.map((skuList: any[]) => {
+				return copyDataSource.find((dataSourceItem) => {
+					const dataSourceSkuList: any[] = dataSourceItem?.skuList || [];
+					return (
+						dataSourceSkuList.length === skuList.length &&
+						skuList.every((it, l) => it?.value === dataSourceSkuList[l]?.value)
+					);
+				});
+			});
+			setDataSource(copyDataSource);
+		}
+	};
+
+	// 添加规格值
+	const handleAddAttrValue = (item: any, index: number) => {
+		const skuData = createSkuData();
+		let startIndex = -1;
+		setDataSource(
+			skuData.map((skuList: any[], i) => {
+				const beforeSkuList = skuList.filter((it) => it.value !== item.value);
+				const oldItem = dataSource.find((dataSourceItem, j) => {
+					const dataSourceSkuList: any[] = dataSourceItem.skuList || [];
+					if (dataSourceSkuList.length === 0) {
+						return true;
+					} else {
+						const isSame =
+							beforeSkuList.length === dataSourceSkuList.length &&
+							beforeSkuList.every(
+								(it, l) => it?.value === dataSourceSkuList[l]?.value
+							);
+						const tempStartIndex = startIndex;
+						startIndex = j;
+						return j > tempStartIndex && isSame;
+					}
+				});
+				return {
+					...oldItem,
+					id: oldItem?.id || randomString(20),
+					skuList,
 				};
-			}
-			return obj;
-		});
-		console.log("newDataSource", newDataSource);
-		setDataSource(newDataSource);
+			})
+		);
+	};
+
+	// 删除规格值
+	const handleDeleteAttrValue = (item: any, index: number) => {
+		const skuData = createSkuData();
+		let newDataSource = [];
+		if (skuData.length === 0) {
+			newDataSource = dataSource.map((item) => {
+				return {
+					...item,
+					skuList: [],
+				};
+			});
+		} else {
+			let startIndex = -1;
+			newDataSource = skuData.map((skuList: any[], i) => {
+				const oldItem = dataSource.find((dataSourceItem, j) => {
+					const dataSourceSkuList: any[] = dataSourceItem.skuList || [];
+					if (dataSourceSkuList.length === 0) {
+						const tempStartIndex = startIndex;
+						startIndex = j;
+						return j > tempStartIndex;
+					} else {
+						const isSame =
+							skuList.length === dataSourceSkuList.length &&
+							skuList.every(
+								(it, l) => it?.value === dataSourceSkuList[l]?.value
+							);
+						const tempStartIndex = startIndex;
+						startIndex = j;
+						return j > tempStartIndex && isSame;
+					}
+				});
+				return {
+					...oldItem,
+					skuList,
+				};
+			});
+		}
+		if (newDataSource.length === 0) {
+			const dataId = randomString(20);
+			setDataSource([
+				{
+					id: dataId,
+					oprice: undefined,
+					cprice: undefined,
+					stock: undefined,
+				},
+			]);
+		} else {
+			setDataSource(newDataSource);
+		}
 	};
 
 	// 校验表格数据
 	const validateFieldsTableData = async () => {
+		await form.validateFields();
 		if (dataSource.length === 0) {
+			const msg = "请至少添加一个规格";
 			setAlertVisile({
 				show: true,
-				msg: "请至少添加一个规格",
+				msg,
 			});
-			return Promise.reject(false);
+			return Promise.reject(msg);
 		}
 		function isEmpty(value: any) {
 			return typeof value === "undefined" || value === "";
 		}
 		let hasEmpty = dataSource.some((item) => {
-			const { oprice, cprice, stock } = item;
-			return isEmpty(oprice) || isEmpty(cprice) || isEmpty(stock);
+			const { oprice, cprice, stock, skuList } = item;
+			return (
+				!Array.isArray(skuList) ||
+				skuList.length === 0 ||
+				skuList.length !== skuKeyList.length ||
+				isEmpty(oprice) ||
+				isEmpty(cprice) ||
+				isEmpty(stock)
+			);
 		});
 		if (hasEmpty) {
+			const msg = "请添加规格项、销售价、成本价、库存";
 			setAlertVisile({
 				show: true,
-				msg: "请填写销售价、成本价、库存",
+				msg,
 			});
-			return Promise.reject(false);
+			return Promise.reject(msg);
 		}
 		setAlertVisile({
 			show: false,
@@ -304,10 +333,11 @@ export default function HomePage() {
 	return (
 		<ProCard>
 			<ProForm
-				formRef={formRef}
 				form={form}
 				layout="horizontal"
-				onValuesChange={(changeValues, allValues) => {}}
+				onValuesChange={(changeValues, allValues) => {
+					console.log(allValues)
+				}}
 				labelCol={{
 					span: 6,
 				}}
@@ -330,7 +360,7 @@ export default function HomePage() {
 									labelInValue: true,
 									onSelect: (item) => handleSelectAttrName(item, index),
 									onChange: () => {
-										formRef.current?.resetFields([formAttrValue]);
+										form?.resetFields([formAttrValue]);
 									},
 								}}
 								request={async () =>
@@ -339,13 +369,20 @@ export default function HomePage() {
 										value: item.value,
 									}))
 								}
+								rules={[
+									{
+										required: true,
+										message: "请选择规格名",
+									},
+								]}
 							/>
 							<ProFormSelect
-								label="规格项"
+								label="规格值"
 								name={formAttrValue}
 								dependencies={[formAttrName]}
 								mode="multiple"
 								fieldProps={{
+									allowClear: false,
 									labelInValue: true,
 									onSelect: (item) => handleAddAttrValue(item, index),
 									onDeselect: (item) => handleDeleteAttrValue(item, index),
@@ -354,6 +391,12 @@ export default function HomePage() {
 									const attrNameValue = params[formAttrName]?.value;
 									return attrValueList(attrNameValue);
 								}}
+								rules={[
+									{
+										required: true,
+										message: "请选择规格值",
+									},
+								]}
 							/>
 							<Button
 								type="primary"
@@ -386,7 +429,6 @@ export default function HomePage() {
 					editableKeys,
 					form: editableForm,
 					onValuesChange: (record, recordList) => {
-						console.log("recordList", recordList);
 						setDataSource(recordList);
 					},
 				}}
